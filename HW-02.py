c005c97249285c4e85323efc8fd3c04a500c7f84
A.Ch <cherednikov77@gmail.com>
2025-12-02 16:29:07 +0200

    to make first step for orders

--- HW-02.py
+++ HW-02.py
@@ -1,5 +1,40 @@
 import queue
 request_queue = queue.Queue()
 request_counter = 0
-print(request_counter)
+def generation_request():
+    global request_counter
+    request_counter +=1
+    new_request = f"Order №{request_counter}"
+    request_queue.put(new_request)
+    print(f"Make and add:{new_request}.Order:{request_counter}")
 
+#####
+def process_request():
+    if not request_queue.empty():
+        request = request_queue.get()
+        print(f"Order processing:{request}")
+        request_queue.task_done()
+        print(f"Order:{request} processing good.")
+    else:
+        print("No orders")
+
+
+
+
+
+
+Функція generate_request():
+    Створити нову заявку
+    Додати заявку до черги
+
+Функція process_request():
+    Якщо черга не пуста:
+        Видалити заявку з черги
+        Обробити заявку
+    Інакше:
+        Вивести повідомлення, що черга пуста
+
+Головний цикл програми:
+    Поки користувач не вийде з програми:
+        Виконати generate_request() для створення нових заявок
+        Виконати process_request() для обробки заявок
