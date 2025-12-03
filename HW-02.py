bc9ed319bb3cb0086a9d8b131180377f9476b086
A.Ch <cherednikov77@gmail.com>
2025-12-03 11:47:24 +0200

    finish(change queue size.

--- HW-02.py
+++ HW-02.py
@@ -1,14 +1,15 @@
 import queue
+
 request_queue = queue.Queue()
 request_counter = 0
-def generation_request():
+def generate_request():
     global request_counter
     request_counter +=1
     new_request = f"Order №{request_counter}"
     request_queue.put(new_request)
-    print(f"Make and add:{new_request}.Order:{request_counter}")
+    print(f"Make and add:{new_request}.Queue size:{request_queue.qsize()}")
 
-#####
+
 def process_request():
     if not request_queue.empty():
         request = request_queue.get()
@@ -18,23 +19,10 @@ def process_request():
     else:
         print("No orders")
 
+generate_request()
+process_request()
 
 
 
 
 
-Функція generate_request():
-    Створити нову заявку
-    Додати заявку до черги
-
-Функція process_request():
-    Якщо черга не пуста:
-        Видалити заявку з черги
-        Обробити заявку
-    Інакше:
-        Вивести повідомлення, що черга пуста
-
-Головний цикл програми:
-    Поки користувач не вийде з програми:
-        Виконати generate_request() для створення нових заявок
-        Виконати process_request() для обробки заявок
