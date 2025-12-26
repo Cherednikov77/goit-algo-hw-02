daa956536b2d94debc79b5c9431b308ee7f1f215
A.Ch <cherednikov77@gmail.com>
2025-12-26 09:29:58 +0000

    bug fixed

--- HW-02-3.py
+++ HW-02-3.py
@@ -4,13 +4,14 @@ def koch_curve(t, order, size):
     if order == 0:
         t.forward(size)
     else:
-        for angle in(60, 120, 60, 0):
+        for angle in(60, -120, 60, 0):
             koch_curve(t, order - 1, size / 3)
             t.left(angle)
 
 def draw_koch_curve(order, size):
     window = turtle.Screen()
     window.bgcolor("white")
+    window.title(f"koch curve({order})")
     t = turtle.Turtle()
     t.speed(0)
     t.penup()
@@ -18,11 +19,15 @@ def draw_koch_curve(order, size):
     t.pendown()
     
     koch_curve(t, order, size)
+    print(The end operation.Pls., closed window and exit)
     window.mainloop()
 def parse_arguments():
     parser = argparse.ArgumentParser(descroption="__init__")
-    args = parser.parser_args()
-    return args
+    parser.add_argument("--order" , type=int, default=3,help="3")
+    parser.add_argument("--size", type=float, default=300.0, help=300.0)
+    
+    return prse_args()
 if __name__ == "__main__":
     args = parse_arguments()
     draw_koch_curve(args.order, args.size)
+    
